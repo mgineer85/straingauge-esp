96de641768d3d033f2fea2b8b6b67dffb1265724
michael <me@mgrl.de>
2023-10-03 22:59:19 +0200

    switch to adafruit nau7802 lib

--- lib/Loadcell/src/Loadcell.cpp
+++ lib/Loadcell/src/Loadcell.cpp
@@ -21,16 +21,23 @@ void LoadcellClass::initialize(void)
 
     this->cbLoadConfiguration(); // Load zeroOffset and calibrationFactor from EEPROM
 
-    nau7802_adc.setChannel(NAU7802_CHANNEL_1);
-    nau7802_adc.setSampleRate(NAU7802_SPS_10); // Increase to max sample rate
-    nau7802_adc.setGain(gain_bits);
-    nau7802_adc.setLDO(NAU7802_LDO_3V0);
+    nau7802_adc.setRate(sample_rate); // Increase to max sample rate
+    nau7802_adc.setGain(gain);
+    nau7802_adc.setLDO(ldo_voltage);
+
+    // Take 10 readings to flush out readings
+    for (uint8_t i = 0; i < 10; i++)
+    {
+        while (!nau7802_adc.available())
+            delay(1);
+        nau7802_adc.read();
+    }
 
     // Re-cal analog front end when we change gain, sample rate, or channel
-    if (!nau7802_adc.calibrateAFE())
+    if (!nau7802_adc.calibrate(NAU7802_CALMOD_INTERNAL))
         log_e("internal ADC calibration failed!");
     else
-        log_i("internal ADC calibration successfull.");
+        log_i("internal ADC calibration successful.");
 
     // register events
 
@@ -84,22 +91,19 @@ void LoadcellClass::cbLoadConfiguration(void)
 void LoadcellClass::postConfigChange(void)
 {
 
-    float sensor_scale_factor = ((float)adc_resolution * gain * (sensor_config.sensitivity)) / (1000.0 * sensor_config.fullrange);
-    int32_t sensor_zero_balance_raw = (int)round(sensor_config.zerobalance * (float)adc_resolution * gain / 1000.0);
+    sensor_scale_factor = ((float)adc_resolution * (float)(1 << gain) * (sensor_config.sensitivity)) / (1000.0 * sensor_config.fullrange);
+    sensor_zero_balance_raw = (int)round(sensor_config.zerobalance * (float)adc_resolution * (float)(1 << gain) / 1000.0);
 
-    nau7802_adc.setCalibrationFactor(sensor_scale_factor);
-    nau7802_adc.setZeroOffset(sensor_zero_balance_raw);
+    log_i("sensor_scale_factor: %0.2f", sensor_scale_factor);
+    log_i("sensor_zero_balance_raw: %i", sensor_zero_balance_raw);
 
-    log_i("Zero offset: %i", nau7802_adc.getZeroOffset());
-    log_i("Calibration factor: %0.2f", nau7802_adc.getCalibrationFactor());
-
-    nau7802_adc.calibrateAFE();
+    nau7802_adc.calibrate(NAU7802_CALMOD_INTERNAL);
 }
 
 // getter for external readout
 int32_t LoadcellClass::getReadingRaw()
 {
-    return nau7802_adc.getReading();
+    return current_reading_raw;
 }
 float LoadcellClass::getReadingDisplayunitFiltered()
 {
@@ -109,20 +113,20 @@ float LoadcellClass::getReadingDisplayunitFiltered()
 // commands triggered externally
 void LoadcellClass::cmdZeroOffsetTare()
 {
-    nau7802_adc.calculateZeroOffset(4); // TODO: needs fix: if 8 averages, Sparkfun lib times out and silently returns 0!
+    sensor_zero_balance_raw = current_reading_raw;
 
-    DataEvent ev("Webservice/sendMessage", "new zero offset: " + String(nau7802_adc.getZeroOffset()));
+    DataEvent ev("Webservice/sendMessage", "new zero offset: " + String(sensor_zero_balance_raw));
     EventManager::instance().publish(ev);
 }
-void LoadcellClass::cmdCalcCalibrationFactor(float weightOnScale)
+void LoadcellClass::cmdCalcCalibrationFactor(float knownReference)
 {
     // step 1: tare
     // step 2: put known weight on scale
     // step 3: calc cal factor
-    nau7802_adc.calculateCalibrationFactor(weightOnScale);
+    // TODO: nau7802_adc.calculateCalibrationFactor(knownReference);
     // step 4: store cal factor
 
-    DataEvent ev("Webservice/sendMessage", "new calibraction factor: " + String(nau7802_adc.getCalibrationFactor()));
+    DataEvent ev("Webservice/sendMessage", "new calibraction factor: " + String(sensor_scale_factor));
     EventManager::instance().publish(ev);
 }
 
@@ -130,6 +134,9 @@ void LoadcellClass::update_loop()
 {
     if (nau7802_adc.available() == true)
     {
-        _readingDisplayunitFiltered.add(nau7802_adc.getWeight(true, 1)); // getWeight means actually receiving the reading divided by the calibration factor to convert to SI-unit. We calibrate for N, so this is it.
+        current_reading_raw = nau7802_adc.read();
+
+        // convert to displayunit: y=(x-b)/m
+        _readingDisplayunitFiltered.add(((current_reading_raw - sensor_zero_balance_raw) / sensor_scale_factor));
     }
 }
--- lib/Loadcell/src/Loadcell.hpp
+++ lib/Loadcell/src/Loadcell.hpp
@@ -3,11 +3,11 @@
 #define AVG_SIZE 8
 
 #include <Arduino.h>
-#include "SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_NAU8702
+// #include "SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_NAU8702
 #include <RunningMedian.h>
 #include <DataEvent.hpp>
 #include <ConfigStructs.hpp>
-// #include <Adafruit_NAU7802.h>
+#include <Adafruit_NAU7802.h>
 
 using namespace esp32m;
 
@@ -21,12 +21,18 @@ private:
         int average;
     };
 
-    NAU7802 nau7802_adc; // Create instance of the NAU7802 class
+    Adafruit_NAU7802 nau7802_adc; // Create instance of the NAU7802 class
     RunningMedian _readingDisplayunitFiltered = RunningMedian(AVG_SIZE);
 
     const int32_t adc_resolution = 1 << 24;
-    const uint8_t gain = 128;
-    const uint8_t gain_bits = NAU7802_GAIN_128;
+    const NAU7802_SampleRate sample_rate = NAU7802_RATE_10SPS;
+    const NAU7802_Gain gain = NAU7802_GAIN_128;
+    const NAU7802_LDOVoltage ldo_voltage = NAU7802_3V0;
+
+    float sensor_scale_factor = 0;
+    int32_t sensor_zero_balance_raw = 0;
+
+    int32_t current_reading_raw = 0;
 
     /* Burster 6005 S/N 668862 */
     // float sensor_fullrange = 5000;
